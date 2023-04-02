804795e942b6811874c761cd76565222faaea4a9
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-04-03 08:39:01 +0900

    feat: multiple state

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const increment2 = () => {
-    setCount((prevCount) => prevCount + 1);
-  };
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
   const reset = () => {
-    setCount(0);
-  };
-
-  const three = () => {
-    setCount((prevCount) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount));
+    setName(props.name);
+    setPrice(props.price);
   };
 
   return (
     <>
-      <div>count: {count}</div>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-      <button onClick={increment2}>increment2</button>
+      <p>
+        現在の{name}は、{price}です
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
       <button onClick={reset}>reset</button>
-      <button onClick={three}>three</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "",
+  price: 1000,
 };
 
 export default App;
