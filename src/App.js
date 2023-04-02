c9b78f99ca3be47294522759ded1df4aabaa4184
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-04-03 08:31:19 +0900

    feat: count

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>test</div>;
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const increment2 = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const three = () => {
+    setCount((prevCount) => (prevCount % 3 === 0 ? prevCount / 3 : prevCount));
+  };
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+      <button onClick={increment2}>increment2</button>
+      <button onClick={reset}>reset</button>
+      <button onClick={three}>three</button>
+    </>
+  );
 };
 
 export default App;
