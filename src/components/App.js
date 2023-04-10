070dacfe8c67048d83c4a490c7755ad8346715f2
yto-tkg <yuto-takagi@cybozu.co.jp>
2023-04-11 08:24:44 +0900

    feat: localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,21 @@ import AppContext from "../contexts/AppContext";
 import reducer from "../reducers/index";
 import EventForm from "./EventForm";
 import Events from "./Events";
+import OperationLogs from "./OperationLogs";
 
 const App = (props) => {
-  const initialState = {
-    events: [],
-    operationLogs: [],
-  };
+  const appState = localStorage.getItem("appWithRedux");
+  const initialState = !!appState
+    ? JSON.parse(appState)
+    : {
+        events: [],
+        operationLogs: [],
+      };
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("appWithRedux", JSON.stringify(state));
+  }, [state]);
 
   return (
     <>
